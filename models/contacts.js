479abed2b6e3c830e39345d4687f5bce04c20668
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-01-29 15:56:50 +0200

    Add functions for read and write file

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,92 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactList = path.join(__dirname, "./contacts.json");
 
-const getContactById = async (contactId) => {}
+const writeToFile = async (body) => {
+  try {
+    const preparedBody = JSON.stringify(body, null, 2);
+    await fs.writeFile(contactList, preparedBody);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const generateId = (array) => {
+  const newId = Number(array[array.length - 1].id) + 1;
+  return String(newId);
+};
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(contactList);
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const currentContact = contacts.find(({ id }) => id === contactId);
+    if (!currentContact) throw new Error("Wrong contact id");
+    return currentContact;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const contactIndex = contacts.findIndex(({ id }) => id === contactId);
+    if (contactIndex === -1) throw new Error("Wrong contact id");
+    contacts.splice(contactIndex, 1);
+    await fs.writeFile(contactList, JSON.stringify(contacts, null, 2));
+    return contacts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addContact = async (body) => {
+  try {
+    const contacts = await listContacts();
+    contacts.push({ id: generateId(contacts), ...body });
+    await writeToFile(contacts);
+    return contacts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    const contacts = await listContacts();
+    const contactIndex = contacts.findIndex(({ id }) => id === contactId);
+    if (contactIndex === -1) throw new Error("Wrong contact id");
+    contacts.splice(contactIndex, 1, { id: contactId, ...body });
+    await writeToFile(contacts);
+    return contacts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// (async () => {
+//   try {
+//     const response = await updateContact("12", {
+//       name: "hello",
+//       email: "kitty",
+//       phone: "777-777",
+//     });
+//     // if (!response) throw new Error("Wrong file name");
+//     console.log("response: ", response);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
 
 module.exports = {
   listContacts,
@@ -16,4 +94,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
